625601b1bf1526ba19f0299455e0e63134d26b7a
Mohsen Mirhoseini Argi <mohsen@karinaco.com>
2016-02-24 10:28:31 +0330

    added exit function to ui

--- app/src/main/java/com/mirhoseini/example/utils/MainActivity.java
+++ app/src/main/java/com/mirhoseini/example/utils/MainActivity.java
@@ -2,6 +2,9 @@ package com.mirhoseini.example.utils;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.mirhoseini.utils.Utils;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void actionExitKillProcess(View view) {
+        Utils.exit();
+    }
+
+    public void actionExitNormal(View view) {
+        Utils.exit(this);
     }
 }
